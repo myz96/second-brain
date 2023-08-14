882c0e197602e2b73040eb9c70bc5183d543febb
Michael Zhao <michael.yj.zhao@gmail.com>
2023-08-14 20:45:14 +1000

    Updating frontend api connection to production website

--- client/vite.config.js
+++ client/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     },
     proxy: {
       "/api": {
-        target: "https://secondbrain-api.onrender.com",
+        target: "https://secondbrain-gptgraph.onrender.com",
         changeOrigin: true,
         secure: false,
       },
